b5785df9f7b6c6a6b4f114a66db07f88634de975
parthsharma1410 <parth.sharma2019a@vitstudent.ac.in>
2021-05-01 22:42:00 +0530

    added firdebase

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+import { AuthProvider } from '../context/AuthContext';
+import Signup from './Signup'
+
+function App() {
+  return (
+    <AuthProvider>
+    <Container className="d-flex align-items-center justify-content-center" style={ {minHeight: "100vh"}}>
+      <div className="w-100" style={{ maxWidth: "400px"}}>
+        <Signup />
+      </div>
+    </Container>
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Signup.js
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { useAuth } from '../context/AuthContext'
+
+function Signup() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+    const { signup } = useAuth()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        if(passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('Passwords do not match')
+        }
+        try {
+            setError('')
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+        } catch {
+            console.log(error)
+            setError('Failed to create an account')
+        }
+        
+        setLoading(false)
+
+    }
+    
+  return (
+    <>
+        <Card>
+            <Card.Body>
+                <h2 className="text-centre mb-4">Sign Up</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" ref={emailRef} required></Form.Control>
+                    </Form.Group>
+                    <Form.Group id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" ref={passwordRef} required></Form.Control>
+                    </Form.Group>
+                    <Form.Group id="password-confirmation">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" ref={passwordConfirmRef} required></Form.Control>
+                    </Form.Group>
+                    <Button disabled={loading} className='w-100' type="submit">Sign Up</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+        <div className='w-100 text-center mt-2'>
+            Already have an account? LogIn
+        </div>
+    </>
+  );
+}
+
+export default Signup;
