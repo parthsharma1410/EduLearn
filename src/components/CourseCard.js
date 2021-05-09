46fe3b34568fe7476fb63300e6e5d76c290795dd
parthsharma1410 <parth.sharma2019a@vitstudent.ac.in>
2021-05-10 01:52:36 +0530

    Enhanced Dashboard

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import courses from "../data/courses.json";
-
+import '../App.css'
 export default function CourseCard(props) {
   var courseNo = props.course;
   var course = courses.courseDetails[courseNo];
@@ -8,13 +8,17 @@ export default function CourseCard(props) {
   return (
     <div className="card">
       <img
-        className="card-img-top"
+        className="card-img"
         src={course.image}
         alt="Card image cap"
       ></img>
       <div className="card-body">
         <strong className="card-text">{course.name}</strong>
         <p className="card-text">{course.university}</p>
+        <div className='flexbox'>
+          <p className='card-text-2'>{course.length}</p>
+          <p className='card-text'>{course.difficulty}</p>
+        </div>
       </div>
     </div>
   );
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -21,19 +21,38 @@ export default function Dashboard() {
     }
   }
   return (
-    <div className="container">
+    <div className="container animate__animated animate__fadeIn">
+       <div className="flexbox">
+        <div className="textcolumn animate__animated animate__fadeIn">
+          {error && <Alert variant="danger">{error}</Alert>}
+          Welcome to EduLearn <strong>{currentUser.email}</strong> !
+        </div>
+        <div className="w-50 text-center mt-2">
+        <button className="btnsubmit2">
+            <a href="/updateprofile">Update Profile</a>
+          </button>
+            <button className="btnsearch" onClick={handleLogout}>
+              Log Out
+            </button>
+          <br /><br />
+          </div>
+        {/* <div className="imagecolumn animate__animated animate__fadeIn animate__delay-1s imgclock">
+          <img src={clock} />
+        </div> */}
+      </div>
       <div className="input-group">
         <input
           type="search"
           className="form-control rounded"
-          placeholder="Search"
+          placeholder="Enter a topic"
           aria-label="Search"
           aria-describedby="search-addon"
         />
-        <button type="button" className="btn btn-outline-primary">
-          search
+        <button type="button" className="btnsearch">
+          Search
         </button>
       </div>
+      <br />
       <div className="courses container">
         <div className="flexbox">
           <CourseCard course={0} />
@@ -46,26 +65,6 @@ export default function Dashboard() {
           <CourseCard course={5} />
         </div>
       </div>
-      <div className="flexbox">
-        <div className="textcolumn animate__animated animate__fadeIn">
-          <h2 className="text-center mb-4 heading">Dashboard</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          Welcome to EduLearn <strong>{currentUser.email}</strong> ! <br /> We
-          are still building the dashboard, <br /> Please visit us later :)
-          <br /> <br />
-          <button className="btnsubmit">
-            <a href="/updateprofile">Update Profile</a>
-          </button>
-          <div className="w-100 text-center mt-2">
-            <Button variant="link" onClick={handleLogout}>
-              Log Out
-            </Button>
-          </div>
-        </div>
-        {/* <div className="imagecolumn animate__animated animate__fadeIn animate__delay-1s imgclock">
-          <img src={clock} />
-        </div> */}
-      </div>
-    </div>
+     </div>
   );
 }
